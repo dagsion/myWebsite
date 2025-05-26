5f197f77c3a469098b0e2b63843d42ca3342a4d1
dagsion <dagsion123@gmail.com>
2025-05-25 17:22:35 -0700

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   css: {
-    modules:{
+    modules: {
       localsConvention: "camelCase",
     },
   },
